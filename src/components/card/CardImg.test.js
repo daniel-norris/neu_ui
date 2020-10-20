0ea848823dbbd946f4f7ea846dcba823d0877459
greyGroot <grey.rsm@gmail.com>
2020-10-20 08:42:07 +0300

    chore: #57 increate test covereage of card img component

--- src/components/card/CardImg.test.js
+++ src/components/card/CardImg.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { render, unmountComponentAtNode } from 'react-dom';
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { render, unmountComponentAtNode } from "react-dom";
 
-import { CardImg } from './CardImg';
+import { CardImg } from "./CardImg";
 
-describe('CardImg test suite', () => {
+describe("CardImg test suite", () => {
   let container = null;
 
   beforeEach(() => {
-    container = document.createElement('div');
+    container = document.createElement("div");
     document.body.appendChild(container);
   });
 
@@ -18,12 +18,21 @@ describe('CardImg test suite', () => {
     container = null;
   });
 
-  it('renders CardImg props', () => {
+  it("renders CardImg props", () => {
     act(() => {
-      render(<CardImg src='test-src' alt='test-alt' />, container);
+      render(<CardImg src="test-src" alt="test-alt" />, container);
     });
 
     expect(document.querySelector('[src="test-src"]')).toBeTruthy();
     expect(document.querySelector('[alt="test-alt"]')).toBeTruthy();
+  });
+
+  it('renders CardImg "alt" prop with default value', () => {
+    act(() => {
+      render(<CardImg src="test-src" />, container);
+    });
+
+    expect(document.querySelector('[src="test-src"]')).toBeTruthy();
+    expect(document.querySelector('[alt="null"]')).toBeTruthy();
   });
 });
