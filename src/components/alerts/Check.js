ebbc100e2b0608822459082d2b4d390da557a39a
Peter Cruckshank <petecruckshank@gmail.com>
2020-12-02 21:23:52 -0500

    Brought in SVG files for component

---
+++ src/components/alerts/Check.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+ export const Check = () => { 
+  return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+        height="28px"
+        width="28px">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+  );
+}
+
+export default Check
---
+++ src/components/alerts/ExclamationCircle.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const ExclamationCircle = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke="#000000"
+      height="28px"
+      width="28px">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export default ExclamationCircle;
